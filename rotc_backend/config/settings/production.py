d8d9c3591398c0a3b5b53a131fccb3e1bf6c2115
MSU-SND <msusndrotcunit@gmail.com>
2026-02-27 01:04:02 +0800

    Add WhiteNoise for serving static files in production

--- rotc_backend/config/settings/production.py
+++ rotc_backend/config/settings/production.py
@@ -108,7 +108,24 @@ SECURE_HSTS_PRELOAD = True
 
 # Static files
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# WhiteNoise configuration for serving static files in production
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Must be after SecurityMiddleware
+    'django.middleware.gzip.GZipMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.authentication.middleware.JWTAuthenticationMiddleware',
+    'apps.system.middleware.PerformanceMonitoringMiddleware',
+    'apps.system.csp_middleware.ContentSecurityPolicyMiddleware',
+]
 
 # Use Cloudinary for media file storage in production
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
