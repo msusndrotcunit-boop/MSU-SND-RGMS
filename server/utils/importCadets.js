a2ce28dd70f6530948b96c2155e8898b3837ef40
MSU-SND <msusndrotcunit@gmail.com>
2026-02-26 15:44:10 +0800

    Clean up repository: Remove old Node.js backend, mobile app, scripts, and outdated documentation

--- server/utils/importCadets.js
+++
@@ -1,9 +0,0 @@
-async function processStaffData(filePath) {
-  return { inserted: 0, updated: 0, errors: [] };
-}
-
-async function processUrlImport(url) {
-  return { inserted: 0, updated: 0, errors: [] };
-}
-
-module.exports = { processStaffData, processUrlImport };
