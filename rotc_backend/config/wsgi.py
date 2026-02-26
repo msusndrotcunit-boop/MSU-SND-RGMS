1840a1d878bd28f86bdfb3dafce61b23acb48c23
MSU-SND <msusndrotcunit@gmail.com>
2026-02-26 14:41:40 +0800

    Add debug logging to WSGI and force production settings when DJANGO_ENV=production

--- rotc_backend/config/wsgi.py
+++ rotc_backend/config/wsgi.py
@@ -8,12 +8,26 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-# Don't use setdefault - let environment variable take precedence
-# This allows DJANGO_SETTINGS_MODULE from render.yaml to be used
-if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+# Force production settings if DJANGO_SETTINGS_MODULE is set to config.settings.production
+# or if DJANGO_ENV is production
+django_settings = os.environ.get('DJANGO_SETTINGS_MODULE', '')
+django_env = os.environ.get('DJANGO_ENV', '')
+
+# Debug logging
+print(f"[WSGI] DJANGO_SETTINGS_MODULE from env: {django_settings}", file=sys.stderr)
+print(f"[WSGI] DJANGO_ENV from env: {django_env}", file=sys.stderr)
+
+if 'production' in django_settings or django_env == 'production':
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.production'
+    print(f"[WSGI] Setting DJANGO_SETTINGS_MODULE to: config.settings.production", file=sys.stderr)
+elif not django_settings:
     os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.development'
+    print(f"[WSGI] Setting DJANGO_SETTINGS_MODULE to: config.settings.development", file=sys.stderr)
+
+print(f"[WSGI] Final DJANGO_SETTINGS_MODULE: {os.environ.get('DJANGO_SETTINGS_MODULE')}", file=sys.stderr)
 
 application = get_wsgi_application()
